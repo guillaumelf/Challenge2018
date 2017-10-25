4b7354f13677db44ba9f0ba1a40bc974c9ee61d9
guillaumelf <guillaume.lefloch56@gmail.com>
2017-10-25 15:51:20 +0200

    new svm.py

--- Python/Pre-processing/svm.py
+++ Python/Pre-processing/svm.py
@@ -9,10 +9,13 @@ Created on Wed Oct 25 13:53:06 2017
 #########################
 
 import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
-from functools import reduce
-from sklearn import datasets, svm, cross_validation, tree, preprocessing, metrics
-from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
+#from concurrent.futures import ThreadPoolExecutor
+#from functools import reduce
+from sklearn import cross_validation, preprocessing
+from sklearn.svm import SVR
+from sklearn.model_selection import GridSearchCV, KFold
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
 ### Définition locale de fonctions
 ##################################
@@ -35,7 +38,7 @@ def preprocess_df(df):
 '''
 On sépare les tâches pour aller plus vite dans l'importation des 36 fichiers csv
 '''
-e = ThreadPoolExecutor()
+# e = ThreadPoolExecutor()
 
 # Exemple pour un fichier
 
@@ -43,13 +46,13 @@ e = ThreadPoolExecutor()
 Préparation des données
 '''
 
-train1 = read_df('data_meteo/train_1.csv')
+train1 = read_df('data_meteo/data_agregated.csv')
 processed_df = preprocess_df(train1)
 
 X = processed_df.drop(['tH2_obs'], axis=1).values
 y = processed_df['tH2_obs'].values
 
-svr_rbf = svm.SVR(kernel='rbf')
+svr_rbf = SVR(kernel='rbf',C=0.01, cache_size=200, coef0=0.0, degree=3, epsilon=0.1, gamma='auto', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
 
 
 '''
@@ -58,17 +61,26 @@ Validation croisée Hold Out
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.33)
 
-svr_rbf.fit(X_train, y_train)
-svr_rbf.score(X_test, y_test)
+mod = svr_rbf.fit(X_train, y_train)
 
 '''
 Validation croisée 10 blocs
 '''
-dict_params = {"C": [.01, .1, 1., 10.]}
-clf = GridSearchCV(estimator=svm.SVR(kernel='rbf'), param_grid=dict_params, cv=KFold(n_splits=10))
-clf.fit(X, y)
+#dict_params = {"C": [.01, .1, 1., 10.]}
+#clf = GridSearchCV(estimator=SVR(kernel='rbf'), param_grid=dict_params, cv=KFold(n_splits=10))
+#clf.fit(X, y)
+#
+#print(clf.best_estimator_)
 
-print(clf.best_estimator_)
+#best_mod = clf.best_estimator_
+#best_mod.fit(X_train, y_train)
+#best_mod.score(X_test, y_test)
+
+pred = mod.predict(X_test)
+
+
+rmse = sqrt(mean_squared_error(y_test, pred))
+
 
 
 
